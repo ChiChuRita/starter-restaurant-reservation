fee6579372a52b686133b18c9c517853a64e92ce
chichurita <ra.singh069@gmail.com>
2021-08-09 18:13:06 +0200

    minor fixes to reservations-controller

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -1,3 +1,10 @@
-exports.seed = function (knex) {
-  return knex.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE");
+const fs = require("fs");
+const path = require("path");
+
+//seeds the reservations table for testing purpose
+exports.seed = function(knex) {
+  return knex('reservations').del()
+    .then(function () {
+      return knex('reservations').insert(JSON.parse(fs.readFileSync(path.join(__dirname, "00-reservations.json"))));
+    });
 };
--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,29 @@
 /**
  * List handler for reservation resources
  */
-async function list(req, res) {
-  res.json({
-    data: [],
-  });
+
+const knex = require("../db/connection");
+const { today } = require("../utils/date-time");
+
+const list = async (req, res) => {
+  try
+  {
+    const dateQuery = req.query.date || today();
+    const entries = await (knex.select().from("reservations").where("reservation_date", dateQuery).orderBy("reservation_time"));
+    res.json(entries);
+  }
+  catch (err)
+  {
+    res.status(400).json({ error: err.message });
+  }
+}
+
+// user story 1
+const createNewOrder = async (req, res) => {
+  console.log(req.body);
+  res.send("NICE!");
 }
 
 module.exports = {
-  list,
+  list, createNewOrder
 };
