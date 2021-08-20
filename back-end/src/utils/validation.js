d8a3e877b3d4b8eccfad4fdd5fb965799ae5eafe
chichurita <ra.singh069@gmail.com>
2021-08-20 15:18:31 +0200

    back-end finished

--- back-end/src/utils/validation.js
+++ back-end/src/utils/validation.js
@@ -4,7 +4,13 @@ const { today, asDateString } = require("./date-time");
 const timePattern = /^\d\d:\d\d$/;
 const mobileNumberPattern = /[+]?[0-9-()]+$/;
 
+/*
+yup is a thrid party libary for validation. This file contains the schemas for objects which will get validated
+yup will automatically throw an exeception if the object is invalid
+*/
+
 //user story 3 time validation checks if the time is not in the past
+//custom validation-function for the reservation_time and reservation_date
 function timeValidation(value, { parent }) {
   const { reservation_date } = parent;
 
@@ -21,6 +27,7 @@ function timeValidation(value, { parent }) {
 
 //user story 3 time validation
 //checks if the time for the reservation is between 10:30 and 21:30 (outside of the time span the resturant is closed)
+//custom validation-function for the reservation_time
 function checkOpenHours(value) {
   if (!value) return false;
 
@@ -35,6 +42,7 @@ function checkOpenHours(value) {
 
 //user story 2 date validation
 //checks if is it not tuesday (tuesdays the resturant is closed)
+//custom validation-function for the reservation_day
 function checkOpenDays(value) {
   return value && value.getDay() != 2;
 }
@@ -44,6 +52,7 @@ const reservationSchema = yup.object().shape({
   first_name: yup.string().max(256).required(),
   last_name: yup.string().max(256).required(),
   mobile_number: yup.string().max(256).matches(mobileNumberPattern).required(),
+  //custom validation-function for the reservation_day (US-02)
   reservation_date: yup
     .date()
     .min(today())
@@ -58,27 +67,34 @@ const reservationSchema = yup.object().shape({
     .max(256)
     .matches(timePattern)
     .required()
+    //custom validation-function for the reservation_time for (US-03)
     .test(
       "timeValid",
       "invalid reservation_date and reservation_time (set time to future)",
       timeValidation
     )
+    //custom validation-function for the reservation_time for (US-03)
     .test(
       "openHours",
       "invalid reservation_date and reservation_time, resturant is closed!",
       checkOpenHours
     ),
+  //reservations must have at least 1 person
   people: yup.number().min(1).required().strict(),
+  //inserted reservation must have the status set to booked
   status: yup
     .string()
     .matches(/(booked)/, ({ value }) => `invalid status: ${value}`),
 });
 
+//formal validation of the table data
+//table name at least 2 characters and capacity at least 1
 const tableSchema = yup.object().shape({
   table_name: yup.string().min(2).max(256).required(),
   capacity: yup.number().min(1).required(),
 });
 
+//schema for mobile_number validation (US-07)
 const numberSchema = yup
   .string()
   .max(256)
--- front-end/src/utils/validation.js
+++ front-end/src/utils/validation.js
@@ -4,7 +4,13 @@ const { today, asDateString } = require("./date-time");
 const timePattern = /^\d\d:\d\d$/;
 const mobileNumberPattern = /[+]?[0-9-()]+$/;
 
+/*
+yup is a thrid party libary for validation. This file contains the schemas for objects which will get validated
+yup will automatically throw an exeception if the object is invalid
+*/
+
 //user story 3 time validation checks if the time is not in the past
+//custom validation-function for the reservation_time and reservation_date
 function timeValidation(value, { parent }) {
   const { reservation_date } = parent;
 
@@ -21,6 +27,7 @@ function timeValidation(value, { parent }) {
 
 //user story 3 time validation
 //checks if the time for the reservation is between 10:30 and 21:30 (outside of the time span the resturant is closed)
+//custom validation-function for the reservation_time
 function checkOpenHours(value) {
   if (!value) return false;
 
@@ -35,6 +42,7 @@ function checkOpenHours(value) {
 
 //user story 2 date validation
 //checks if is it not tuesday (tuesdays the resturant is closed)
+//custom validation-function for the reservation_day
 function checkOpenDays(value) {
   return value && value.getDay() !== 2;
 }
@@ -48,27 +56,39 @@ export const reservationSchema = yup.object().shape({
     .max(256)
     .matches(mobileNumberPattern, "No valid phone number")
     .required("Required"),
+  //custom validation-function for the reservation_day (US-02)
   reservation_date: yup
     .date()
     .min(today(), "Date is in the past")
     .test("openDays", "Resturant is not open", checkOpenDays)
     .required("Required")
     .typeError("Must be a valid date"),
+  //custom validation-function for the reservation_time for (US-03)
   reservation_time: yup
     .string()
     .max(256)
     .matches(timePattern, "No valid Time")
     .required("Required")
+    //custom validation-function for the reservation_time for (US-03)
     .test("timeValid", "Time is in the past", timeValidation)
+    //custom validation-function for the reservation_time for (US-03)
     .test("openHours", "Resturant is not open", checkOpenHours),
+  //reservations must have at least 1 person
   people: yup.number().min(1).required().typeError("Must be a number"),
+  //inserted reservation must have the status set to booked
+  status: yup
+    .string()
+    .matches(/(booked)/, ({ value }) => `invalid status: ${value}`),
 });
 
+//formal validation of the table data
+//table name at least 2 characters and capacity at least 1
 export const tableSchema = yup.object().shape({
   table_name: yup.string().min(2).max(256).required("Required"),
   capacity: yup.number().min(1).required().typeError("Must be a number"),
 });
 
+//schema for mobile_number validation (US-07)
 export const searchSchema = yup.object().shape({
   mobile_number: yup
     .string()
